d809d9f7779f5337d20c886038efc0efa9089c99
Kriti Banka <bankakriti10@gmail.com>
2023-06-29 11:02:59 +0530

    otp and user-login api

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:collegevidya/Screens/Experts.dart';
 import 'package:collegevidya/Screens/Navigation.dart';
-import 'package:collegevidya/Screens/Signin.dart';
-import 'package:collegevidya/Screens/Signup.dart';
+import 'package:collegevidya/Screens/Signin-Signup/Signin.dart';
+import 'package:collegevidya/Screens/Signin-Signup/Signup.dart';
 import 'package:collegevidya/Screens/Splash%20Screen.dart';
 import 'package:collegevidya/Screens/add%20post.dart';
-import 'package:collegevidya/Screens/completeprofile.dart';
 import 'package:collegevidya/Screens/verifyUniversity%20.dart';
 import 'package:flutter/material.dart';
 import 'Screens/Suggestin2min.dart';
 
 void main(){
+
 
   runApp(
     MaterialApp(
@@ -18,7 +18,6 @@ void main(){
       routes: {
         '/navigation': (context) => navigation(),
         "/signup": (context) => signup(),
-        "/completeprofile": (context) => completeprofile(),
         "/signin": (context) => signin(),
         "/suggest": (context) => suggest(),
         '/addpost': (context) => addpost(),
